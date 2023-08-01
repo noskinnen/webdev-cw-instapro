fb1b88615c8be13cb7790e22b4856baf60b1f1fe
Andreas Noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-08-01 11:18:22 +0300

    course2 update 01.02.2023
    course2 update 01.02.2023

--- helpers.js
+++ helpers.js
@@ -43,4 +43,13 @@ export function getUpdateLikes(arr){
   }
 }
 
-
+export const dataFormat = (commentDate) => {
+  const dateComment = new Date(commentDate);
+  const formatDate =
+    dateComment.getDate().toString().padStart(2, '0') + '.' +
+    (dateComment.getMonth() + 1).toString().padStart(2, '0') + '.' +
+    dateComment.getFullYear().toString().slice(-2) + ' ' +
+    dateComment.getHours().toString().padStart(2, '0') + ':' +
+    dateComment.getMinutes().toString().padStart(2, '0');
+    return formatDate;
+  }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getPosts, sendPosts } from "./api.js";
+import { getPosts, sendPosts, userPosts } from "./api.js";
 import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
 import { renderAuthPageComponent } from "./components/auth-page-component.js";
 import {
@@ -70,7 +70,9 @@ export const goToPage = (newPage, data) => {
     if (newPage === USER_POSTS_PAGE) {
       // TODO: реализовать получение постов юзера из API
       console.log("Открываю страницу пользователя: ", data.userId);
-      return getPosts({ token: getToken() })
+      page = USER_POSTS_PAGE;
+      renderApp();
+      return userPosts({userId: data.userId, token: getToken() })
       .then((newPosts) => {
         page = USER_POSTS_PAGE;
         posts = newPosts;
